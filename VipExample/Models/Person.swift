cc1ba69f1f9f514423b9b5b8fc55db1a3d023e89
Thiago Valente <thiagoavalente@icloud.com>
2019-09-27 10:48:18 -0300

    Show List table

---
+++ VipExample/Models/Person.swift
@@ -0,0 +1,14 @@
+//
+//  Person.swift
+//  VipExample
+//
+//  Created by Thiago Valente on 26/09/19.
+//  Copyright © 2019 Thiago Valente. All rights reserved.
+//
+
+import Foundation
+
+struct Person {
+    var name: String
+    var salary: Double
+}
